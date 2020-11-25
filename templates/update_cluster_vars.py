0f4058c2e03dc3c167c69bce0b04de5f578f94f8
Cody Hill <15899666+c0dyhi11@users.noreply.github.com>
2020-11-25 13:18:48 -0600

    Latest GKE version plus private key written to disk and no more master verbage (#18)
    - This has been updated to `0.7.0-gke.4`
    - The SSH Private key is now written to `~/.ssh/<unique_name>`
    - All of the verbiage for `master` has been changed to `cp` or `control-plane`

--- templates/update_cluster_vars.py
+++ templates/update_cluster_vars.py
@@ -4,18 +4,18 @@ import os
 
 # Terraform VARs
 PRIV_SUBNET = '${private_subnet}'
-NUM_MASTERS = ${master_count}
+NUM_CPS = ${cp_count}
 NUM_WORKERS = ${worker_count}
 CLUSTER_NAME = '${cluster_name}'
 
-num_total = NUM_MASTERS + NUM_WORKERS
-master_string = ''
+num_total = NUM_CPS + NUM_WORKERS
+cp_string = ''
 worker_string = ''
 
-for i in range(0, NUM_MASTERS):
-    master_string += "      - address: {}\n".format(list(ipaddress.ip_network(PRIV_SUBNET).hosts())[i].compressed)
+for i in range(0, NUM_CPS):
+    cp_string += "      - address: {}\n".format(list(ipaddress.ip_network(PRIV_SUBNET).hosts())[i].compressed)
 
-for i in range(NUM_MASTERS, num_total):
+for i in range(NUM_CPS, num_total):
     worker_string += "  - address: {}\n".format(list(ipaddress.ip_network(PRIV_SUBNET).hosts())[i].compressed)
 
 cluster_vip = list(ipaddress.ip_network(PRIV_SUBNET).hosts())[-1].compressed
@@ -26,7 +26,7 @@ lb_vip_range = "{}-{}".format(lb_vip_start, lb_vip_end)
 
 file_path = "/root/baremetal/bmctl-workspace/{0}/{0}.yaml".format(CLUSTER_NAME)
 # String Replacements
-os.system("sed -i 's|      - address: <Machine 1 IP>|{}|g' {}".format(master_string,
+os.system("sed -i 's|      - address: <Machine 1 IP>|{}|g' {}".format(cp_string,
           file_path).encode("unicode_escape").decode("utf-8"))
 os.system("sed -i 's|  - address: <Machine 2 IP>|{}|g' {}".format(worker_string,
           file_path).encode("unicode_escape").decode("utf-8"))
