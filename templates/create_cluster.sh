d541fc820d2845eada4979b3bb76a89306b138df
Josh Padilla <joshpadilla@gmail.com>
2021-03-29 22:15:26 +0000

    Rename cluster_create.log to bmctl_create_cluster_stdout.log

--- templates/create_cluster.sh
+++ templates/create_cluster.sh
@@ -6,5 +6,5 @@ export GOOGLE_APPLICATION_CREDENTIALS=/root/baremetal/keys/bmctl.json
 GREEN='\033[0;32m' # Color green
 NC='\033[0m' # No Color
 printf "$${GREEN}Creating Anthos Cluster. This will take about 20 minutes...$${NC}\n"
-/root/baremetal/bmctl create cluster -c $CLUSTER_NAME --force &> /root/baremetal/cluster_create.log
+/root/baremetal/bmctl create cluster -c $CLUSTER_NAME --force &> /root/baremetal/bmctl_create_cluster_stdout.log
 printf "$${GREEN}Cluster Created!$${NC}\n"
